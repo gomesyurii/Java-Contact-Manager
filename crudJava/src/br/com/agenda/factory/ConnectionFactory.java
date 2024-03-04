5c2333ec0b8ead7eb0fbc400910a054060829f18
Yuri de Jesus Gomes <dev.yurigomes@gmail.com>
2024-03-04 15:03:25 -0300

    factory

---
+++ crudJava/src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,14 @@
+package br.com.agenda.factory;
+
+public class ConnectionFactory {
+
+	//Nome do usuário do BD
+	private static final String USERNAME = "root";
+	
+	//Senha do BD
+	private static final String PASSWORD = "123321";
+	
+	//Caminho BD
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda";
+	
+}
