0bb1454340f1b6aacaef90ce27d5a2d47a35c7bb
Yuri de Jesus Gomes <dev.yurigomes@gmail.com>
2024-03-04 15:53:09 -0300

    DAO

--- crudJava/src/br/com/agenda/dao/ContatoDAO.java
+++ crudJava/src/br/com/agenda/dao/ContatoDAO.java
@@ -1,12 +1,56 @@
 package br.com.agenda.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import br.com.agenda.factory.ConnectionFactory;
+import br.com.agenda.model.Contato;
+
 public class ContatoDAO {
 
 	
 	
-	public void save(ContatoDAO contato) {
+	public void save(Contato contato) {
 		
+		String sql = "INSERT INTO contatos(nome, idade, dataCadastro) VALUES (?, ?, ?)";
 		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			
+			//Add valores esperados para query
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			pstm.setString(1, contato.getNome());
+			pstm.setInt(2, contato.getIdade());
+			pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+			
+			pstm.execute();
+			
+			
+		} 
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				if(pstm!=null) {
+					pstm.close();
+					
+				}
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 		
 	}
 	
--- crudJava/src/module-info.java
+++ crudJava/src/module-info.java
@@ -6,4 +6,5 @@
  */
 module crudJava {
 	requires java.sql;
+	requires mysql.connector;
 }
