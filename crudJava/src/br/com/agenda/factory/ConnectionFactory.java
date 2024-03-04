e40ce31789a2b51560016994bb5925c41fa73fca
Yuri de Jesus Gomes <dev.yurigomes@gmail.com>
2024-03-04 15:29:32 -0300

    Update ConnectionFactory.java

--- crudJava/src/br/com/agenda/factory/ConnectionFactory.java
+++ crudJava/src/br/com/agenda/factory/ConnectionFactory.java
@@ -1,8 +1,7 @@
 package br.com.agenda.factory;
 
-import java.sql.Connection; 
+import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class ConnectionFactory {
 
