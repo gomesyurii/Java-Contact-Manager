01ab0b6d8a86571e96bad7e96097647f4d9a9a46
Yuri de Jesus Gomes <dev.yurigomes@gmail.com>
2024-03-05 14:47:16 -0300

    Update ContatoDAO.java

--- crudJava/src/br/com/agenda/dao/ContatoDAO.java
+++ crudJava/src/br/com/agenda/dao/ContatoDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 
 import java.sql.Date;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,65 +14,99 @@ import br.com.agenda.model.Contato;
 
 public class ContatoDAO {
 
-	
-	
 	public void save(Contato contato) {
-		
+
 		String sql = "INSERT INTO contatos(nome, idade, dataCadastro) VALUES (?, ?, ?)";
-		
+
 		Connection conn = null;
 		ClientPreparedStatement pstm = null;
-		
+
 		try {
 			conn = ConnectionFactory.createConnectionToMySQL();
-			
-			 
-			//Add valores esperados para query
+
+			// Add valores esperados para query
 			pstm = (ClientPreparedStatement) conn.prepareStatement(sql);
 			pstm.setString(1, contato.getNome());
 			pstm.setInt(2, contato.getIdade());
 			pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
-			
+
 			pstm.execute();
-			
-			System.out.print("Contato salvo"); 
-			
-		} 
-		
-		catch(Exception e){
+
+			System.out.print("Contato salvo");
+
+		}
+
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		finally {
 			try {
-				if(pstm!=null) {
+				if (pstm != null) {
 					pstm.close();
-					
+
 				}
-				if(conn!=null) {
+				if (conn != null) {
 					conn.close();
 				}
-			} catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
-	public List<Contato> getContatos() {
-		
+
+	public List<Contato> getContatos(){
+
 		String sql = "SELECT * FROM contatos";
-		
+
 		List<Contato> contatos = new ArrayList<Contato>();
-		
+
 		Connection conn = null;
 		ClientPreparedStatement pstm = null;
-		
-		
 		ResultSet rset = null;
-		
+
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+
+			// Add valores esperados para query
+			pstm = (ClientPreparedStatement) conn.prepareStatement(sql);
+
+			rset = pstm.executeQuery();
+
+			while (rset.next()) {
+				Contato contato = new Contato();
+				contato.setId(rset.getInt("id"));
+				contato.setNome(rset.getString("nome"));
+				contato.setIdade(rset.getInt("idade"));
+				contato.setDataCadastro(rset.getDate("dataCadastro"));
+
+				contatos.add(contato);
+			}
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rset != null) { 
+					rset.close();
+				}
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+			
+		}
 		return contatos;
-		
+
 	}
-	
+
 }
